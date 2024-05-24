be47c81a0ccf4974e026bc17ae6cd405530f3d4e
christian-iuil <okoriec01@gmail.com>
2024-05-24 13:50:57 +0100

    update

--- lib/features/auth/controller/auth_controller.dart
+++ lib/features/auth/controller/auth_controller.dart
@@ -2,7 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reddist_clone_app/features/auth/repo/auth_repo.dart';
 
 final authControllerProvider = Provider(
-    (ref) => AuthController(authRepoistory: ref.watch(authRepoProvider)));
+    (ref) => AuthController(authRepoistory: ref.read(authRepoProvider)));
 
 class AuthController {
   final AuthRepoistory _authRepoistory;

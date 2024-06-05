411e70834324f38d5d017c9c191ec21b1f983397
christian-iuil <okoriec01@gmail.com>
2024-06-05 13:02:37 +0100

    update

--- lib/core/common/sigin_button.dart
+++ lib/core/common/sigin_button.dart
@@ -31,3 +31,21 @@ class SignInButtion extends ConsumerWidget {
     );
   }
 }
+
+// A shared state that can be accessed by multiple widgets at the same time.
+// @riverpod
+// class Count extends _$Count {
+//   @override
+//   int build() => 0;
+
+//   void increment() => state++;
+// }
+
+// // Consumes the shared state and rebuild when it changes
+// class Title extends ConsumerWidget {
+//   @override
+//   Widget build(BuildContext context, WidgetRef ref) {
+//     final count = ref.watch(countProvider);
+//     return Text('$count');
+//   }
+// }
